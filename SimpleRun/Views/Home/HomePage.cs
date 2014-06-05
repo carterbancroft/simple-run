909052ffd90600176f5c6fe3eb65195700ce0e6a
Carter Bancroft <alt234@gmail.com>
2014-06-04 21:09:29 -0800

    Use relative layout and add some animation.

--- SimpleRun/Views/Home/HomePage.cs
+++ SimpleRun/Views/Home/HomePage.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading;
 using System.Reactive.Linq;
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace SimpleRun.Views.Home
 {
@@ -12,87 +13,134 @@ namespace SimpleRun.Views.Home
 		Label distanceLabel;
 		Label paceLabel;
 		Label durationLabel;
+		Label referenceLabel;
 		DateTimeOffset startTime;
 		GeoLocationTracker tracker;
+		RelativeLayout layout;
+
+		IDisposable distanceAndSpeedSubscription;
+		IDisposable durationSubscription;
 
 		public HomePage()
 		{
 			tracker = new GeoLocationTracker();
+			layout = new RelativeLayout();
+			BackgroundColor = App.StationaryTint;
+
+			runButton = new Button {
+				Text = "Run",
+				TextColor = Color.White,
+				Font = Font.SystemFontOfSize(50),
+				BackgroundColor = App.RunTint,
+				BorderRadius = 80,
+			};
+
+			// A Label whose upper-left is centered vertically and is simply used as a reference for layouts.
+			referenceLabel = new Label
+			{
+				Text = "Run",
+				Opacity = 0,
+				Font = Font.SystemFontOfSize(50)
+			};
 
 			distanceLabel = new Label {
-				Text = "0.00 km",
 				TextColor = Color.White,
 				XAlign = TextAlignment.Center,
 				Font = Font.SystemFontOfSize(50),
 			};
 
 			paceLabel = new Label {
-				Text = "00:00 per km",
 				TextColor = Color.White,
 				XAlign = TextAlignment.Center,
 				Font = Font.SystemFontOfSize(20),
 			};
 
 			durationLabel = new Label {
-				Text = "00:00:00",
 				TextColor = Color.White,
 				XAlign = TextAlignment.Center,
 				Font = Font.SystemFontOfSize(20),
 			};
 
-			runButton = new Button {
-				TextColor = Color.White,
-				Font = Font.SystemFontOfSize(55),
-			};
+			InitLabels();
 
-			var distanceTimer = Observable.Interval(TimeSpan.FromSeconds(2)).DistinctUntilChanged();
-			var durationTimer = Observable.Interval(TimeSpan.FromSeconds(1)).DistinctUntilChanged();
-
-			IDisposable distanceAndSpeedSubscription = null;
-			IDisposable durationSubscription = null;
+			distanceAndSpeedSubscription = null;
+			durationSubscription = null;
 
 			runButton.Clicked += (sender, e) => {
-				App.UserIsRunning = !App.UserIsRunning;
-
-				SetUIState();
-
-				if (App.UserIsRunning) {
-					tracker.BeginTrackingLocation();
-					startTime = DateTimeOffset.Now;
-					distanceAndSpeedSubscription = distanceTimer.ObserveOn(SynchronizationContext.Current).Subscribe(DistanceAndSpeedTimerTick);
-					durationSubscription = durationTimer.ObserveOn(SynchronizationContext.Current).Subscribe(DurationTimerTick);
-				} else {
-					tracker.StopTrackingLocation();
-					distanceAndSpeedSubscription.Dispose();
-					durationSubscription.Dispose();
-				}
+				if (!App.UserIsRunning)
+					StartRun();
+				else
+					StopRun();
 			};
 
-			Content = new StackLayout {
-				Orientation = StackOrientation.Vertical,
-				Children = {
-					new StackLayout {
-						VerticalOptions = LayoutOptions.Start,
-						Padding = new Thickness(0, 70, 0, 0),
-						Children = {
-							runButton
-						}
-					},
-					new StackLayout {
-						VerticalOptions = LayoutOptions.CenterAndExpand,
-						Children = {
-							distanceLabel,
-							paceLabel,
-							durationLabel,
-						}
-					},
-				}
-			};
+			layout.Children.Add(
+				referenceLabel,
+				Constraint.RelativeToParent(parent => {
+					return parent.Width / 2;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Height / 2;
+				})
+			);
+
+			layout.Children.Add(
+				runButton,
+				Constraint.RelativeToParent(parent => {
+					return (parent.Width / 2) / 2;
+				}),
+				Constraint.RelativeToParent(parent => {
+					if (App.UserIsRunning)
+						return (parent.Width / 2) - 125;
+					
+					return parent.Width / 2;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Width / 2;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Width / 2;
+				})
+			);
+
+			var padding = 5;
+
+			layout.Children.Add(
+				distanceLabel,
+				Constraint.Constant(0),
+				Constraint.RelativeToView(referenceLabel, (parent, sibling) => {
+					return sibling.Y - sibling.Height / 2;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Width;
+				})
+			);
+
+			layout.Children.Add(
+				paceLabel,
+				Constraint.Constant(0),
+				Constraint.RelativeToView(distanceLabel, (parent, sibling) => {
+					return sibling.Y + referenceLabel.Height;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Width;
+				})
+			);
+
+			layout.Children.Add(
+				durationLabel,
+				Constraint.Constant(0),
+				Constraint.RelativeToView(paceLabel, (parent, sibling) => {
+					return sibling.Y + paceLabel.Height + padding;
+				}),
+				Constraint.RelativeToParent(parent => {
+					return parent.Width;
+				})
+			);
+
+			Content = layout;
 
 			Icon = "pin@2x.png";
 			Title = "Run";
-
-			SetUIState();
 		}
 
 		void DistanceAndSpeedTimerTick(long l)
@@ -114,10 +162,51 @@ namespace SimpleRun.Views.Home
 			durationLabel.Text = currentTimespan.ToString(@"hh\:mm\:ss");
 		}
 
-		void SetUIState()
+		async Task StartRun()
 		{
-			BackgroundColor = App.UserIsRunning ? App.RunTint : App.StationaryTint;
-			runButton.Text = App.UserIsRunning ? "Stop" : "Run";
+			App.UserIsRunning = true;
+
+			await runButton.LayoutTo(new Rectangle(runButton.X, runButton.Y - 125, runButton.Width, runButton.Height), 150, Easing.SpringOut);
+		
+			var distanceTimer = Observable.Interval(TimeSpan.FromSeconds(2)).DistinctUntilChanged();
+			var durationTimer = Observable.Interval(TimeSpan.FromSeconds(1)).DistinctUntilChanged();
+
+			tracker.BeginTrackingLocation();
+
+			runButton.Text = "Stop";
+			BackgroundColor = App.RunTint;
+			distanceLabel.Opacity = 1;
+			paceLabel.Opacity = 1;
+			durationLabel.Opacity = 1;
+
+			startTime = DateTimeOffset.Now;
+			distanceAndSpeedSubscription = distanceTimer.ObserveOn(SynchronizationContext.Current).Subscribe(DistanceAndSpeedTimerTick);
+			durationSubscription = durationTimer.ObserveOn(SynchronizationContext.Current).Subscribe(DurationTimerTick);
+		}
+
+		async Task StopRun()
+		{
+			InitLabels();
+
+			tracker.StopTrackingLocation();
+			distanceAndSpeedSubscription.Dispose();
+			durationSubscription.Dispose();
+
+			await runButton.LayoutTo(new Rectangle(runButton.X, runButton.Y + 125, runButton.Width, runButton.Height), 150, Easing.SpringOut);
+			App.UserIsRunning = false;
+			BackgroundColor = App.StationaryTint;
+			runButton.Text = "Run";
+		}
+
+		void InitLabels()
+		{
+			distanceLabel.Text = "0.00 km";
+			paceLabel.Text = "00:00 per km";
+			durationLabel.Text = "00:00:00";
+
+			distanceLabel.Opacity = 0;
+			paceLabel.Opacity = 0;
+			durationLabel.Opacity = 0;
 		}
 	}
 }
